f6bfb53467a293bc8c0587ab084f455c67e84116
Matheus Pohren Oliveira <127795081+MatheusPohren@users.noreply.github.com>
2023-03-31 19:45:08 -0300

    Initial commit

---
+++ HostCode/js/script.js
@@ -0,0 +1,10 @@
+//var n1 = Number.parseInt(window.prompt('Digite um número'))
+//var n2 = Number(window.prompt('Digite outro número'))
+//var s = n1 + n2
+//window.alert(`${n1} + ${n2} = ${s}`)
+
+//var nick_discord = window.prompt('Digite seu nick do Discord')
+//window.alert('Nick registrado, ' + nick_discord + '!')
+//var id_discord = window.prompt('Digite seu ID do Discord')
+//window.alert(`ID Registrado [${id_discord}]`)  // `` para ${}
+//document.write(p)
